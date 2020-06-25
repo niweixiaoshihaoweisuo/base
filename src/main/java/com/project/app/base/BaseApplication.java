a3200c53cac3a91a3739568a3b72c47e1bc44a5e
12675-李冬亮 <dongliang.li@hand-china>
2020-06-25 23:46:35 +0800

    Initial commit

---
+++ src/main/java/com/project/app/base/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.project.app.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+}
